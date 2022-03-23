62b504fed80fa12810d4b9d479d142ac3b02aa45
MrYannick29 <yannick.saelen@gmail.com>
2022-03-23 14:46:07 +0100

    opencv code in subscriber node

--- src/package_opencv/package_opencv/subscriber_member_function.py
+++ src/package_opencv/package_opencv/subscriber_member_function.py
@@ -14,9 +14,40 @@
 
 import rclpy
 from rclpy.node import Node
+import cv2
 
 from std_msgs.msg import String
 
+class Cascade_filter():
+    def __init__(self, number):
+        try:
+            if(number<0 or number>2):
+                raise Exception("Value has to be between 0 and 2")
+            if(number == 0):
+                self.detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
+            if (number == 1):
+                self.detector= cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_upperbody.xml")
+            if (number == 2):
+                self.detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_lowerbody.xml")
+        except Exception as er:
+            print("Value has to be between 0 and 2")
+
+    def detect(self, gray_frame):
+        detected_upper = self.detector.detectMultiScale3(gray_frame, outputRejectLevels=True)
+        rects, weights, score = detected_upper
+
+        return rects
+
+class Full_body_detector():
+
+    def __init__(self, ):
+        self.full_body_detect = cv2.HOGDescriptor()
+        self.full_body_detect.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    def detect_full_body(self, gray_frame):
+        detected_persons = self.full_body_detect.detectMultiScale(gray_frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        rects, weights = detected_persons
+        return rects
 
 class MinimalSubscriber(Node):
 
@@ -28,9 +59,27 @@ class MinimalSubscriber(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        self.full_body_detector=Full_body_detector()
+        self.face_detector=Cascade_filter(0)
+        self.upper_body_detector = Cascade_filter(1)
+        self.lower_body_detector = Cascade_filter(2)
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def listener_callback(self, frame):
+
+        while True:
+            frame=cv2.resize(frame, (320, 240), interpolation = cv2.INTER_AREA)
+            gray=cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+            #Detect Persons
+            rects_full_body=self.full_body_detector.detect_full_body(gray)
+            #Detect Faces
+            rects_faces=self.face_detector.detect(gray)
+            #Upper body
+            rects_upper_body=self.upper_body_detector.detect(gray)
+            #Lower body
+            rects_lower_body=self.lower_body_detector.detect(gray)
+            
+            #Hier moet je message publishen
 
 
 def main(args=None):
