19f53165bbf218af09b68d0cbab6ba35c13504d5
Nicololmen <schepers.milan@gmail.com>
2022-03-23 14:38:18 +0100

    Changes Made For Yannick

--- StalkBotEyes.py
+++ StalkBotEyes.py
@@ -3,28 +3,8 @@ import cv2
 from cascade_filter import Cascade_filter
 from full_body_detector import Full_body_detector
 
-def drawRectangles(frame, rects, color):
-    for idx, (column, row, width, height) in enumerate(rects):
-        cv2.rectangle(
-            frame,
-            (column, row),
-            (column + width, row + height),
-            color,
-            2
-        )
-    return frame
 
 
-def determine_action(rects_faces, rects_full_body, rects_upper_body, rects_lower_body):
-    action="Niks"
-    if (len(rects_full_body) == 0 and len(rects_faces) == 0 and len(rects_upper_body) == 0):
-        action="Zoeken"
-    if ((len(rects_full_body) != 0 or len(rects_upper_body) != 0) and len(rects_faces) == 0):
-        action="Rijden"
-    if (len(rects_faces) != 0):
-        action="Stoppen"
-
-    return action
 
 def main():
     capture = cv2.VideoCapture(0)
@@ -41,29 +21,15 @@ def main():
 
         #Detect Persons
         rects_full_body=full_body_detector.detect_full_body(gray)
-        frame=drawRectangles(frame, rects_full_body, (255,0,0))
-
         #Detect Faces
         rects_faces=face_detector.detect(gray)
-        frame=drawRectangles(frame, rects_faces, (0,255,0))
-
         #Upper body
         rects_upper_body=upper_body_detector.detect(gray)
-        frame=drawRectangles(frame, rects_upper_body, (0,0,255))
-
         #Lower body
         rects_lower_body=lower_body_detector.detect(gray)
-        frame = drawRectangles(frame, rects_lower_body, (0, 255, 255))
-
-        action=determine_action(rects_faces, rects_full_body, rects_upper_body, rects_lower_body)
-
-        cv2.putText(frame, action, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color=(255, 255, 0))
-
-        cv2.imshow('Frame',frame)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-    capture.release()
-    cv2.destroyAllWindows()
+        
+        #Hier moet je message publishen
+    
 
 
 
